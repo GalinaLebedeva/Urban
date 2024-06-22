f8094e509f530a7bbc44aa51c7493e17a2ff8c02
Galina <116099759+GalinaLebedeva@users.noreply.github.com>
2024-06-22 19:18:09 +0300

    Update module_5_1.py
    Добавлены два класса, в одном из которых переопределяется атрибут, в зависимости от введенного числа, а во второй перегружается оператор equal

--- module_5_1.py
+++ module_5_1.py
@@ -18,3 +18,48 @@ h1 = House('ЖК Камчатка', 18)
 h2 = House('Дом в Луйсковицах', 2)
 h1.go_to(7)
 h2.go_to(3)
+
+h1 = House('ЖК Камчатка', 18)
+print(h1.name, h1.number_of_floors)
+h2 = House('Дом в Луйсковицах', 2)
+h1.go_to(5)
+h2.go_to(3)
+
+#Задача с переопределением атрибута Количество этажей
+
+class House2 ():
+
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors (self, floors):
+        self.setNewNumberOfFloors = floors
+        self.numberOfFloors = self.setNewNumberOfFloors
+
+h1 = House2()
+print(h1.numberOfFloors)
+h1.setNewNumberOfFloors(2)
+print(h1.numberOfFloors)
+
+# Задача с перегрузкой оператора equal
+
+class Building():
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = int(numberOfFloors)
+        self.buildingType = str(buildingType)
+
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
+
+    def __str__(self):
+        return f'Число этажей: {self.numberOfFloors}, Тип здания: {self.buildingType}'
+
+B1 = Building(2, 'private house')
+B2 = Building(2, 'private house')
+print(B1)
+print(B2)
+print(B1 == B2)
+B1.numberOfFloors = 3
+print(B1)
+print(B2)
+print(B1 == B2)
