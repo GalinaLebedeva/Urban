b4db407839bddb743173936a69d5ae49d8998401
Galina <116099759+GalinaLebedeva@users.noreply.github.com>
2024-07-03 20:19:19 +0300

    Update module_6_3.py
    Изменен принцип наследования, чтобы пегас говорил звук орла, а не лошади

--- module_6_3.py
+++ module_6_3.py
@@ -5,31 +5,40 @@
 # Pegasus - класс описывающий пегаса. Наследуется от Horse и Eagle в том же порядке.
 
 class Horse():
-    x_distance = 0
-    sound = 'Frrr'
+
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frr'
+        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
 
+
 class Eagle():
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+
+    def __init__(self):
+        super().__init__()
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
 
-class Pegasus(Horse,Eagle):
 
+class Pegasus(Horse, Eagle):
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
     def get_pos(self):
-        return (self.x_distance, self.y_distance)
+        return self.x_distance, self.y_distance
 
     def voice(self):
-        print(Eagle.sound)
+        print(self.sound)
 
+
+print(Pegasus.mro())
 p1 = Pegasus()
 
 print(p1.get_pos())
