84aed58436d11d3e4f0b960efc8c924ef2d12f7a
Galina <116099759+GalinaLebedeva@users.noreply.github.com>
2024-08-10 13:06:44 +0300

    Создание функций на лету
    Использование lambda-функции, замыкания функции и метода класса __call__

---
+++ module_9_4.py
@@ -0,0 +1,42 @@
+# Задача "Функциональное разнообразие":
+import random
+from random import choice
+
+# Lambda-функция:
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+res = list(map(lambda x,y: x == y, first, second))
+print(res)
+
+# Замыкание:
+
+def get_advanced_writer(file_name):
+
+    def write_everything(*data_set):
+
+        with open(file_name, 'w', encoding= 'utf-8') as file:
+            for elem in data_set:
+                file.write(str(elem) + '\n')
+
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Метод __call__:
+
+class MysticBall:
+
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        item = random.choice(self.words)
+        return item
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
