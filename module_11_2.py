6104008d9d2a15843ae0ef2866094ad76bc3d250
Galina <116099759+GalinaLebedeva@users.noreply.github.com>
2024-09-02 13:23:14 +0300

    Update module_11_2.py
    Изменена проверка атрибутов объекта

--- module_11_2.py
+++ module_11_2.py
@@ -4,20 +4,19 @@ from pprint import pprint
 
 def introspection_info(obj):
     info = {}
-    atr =[]
+    atr = []
     method = []
     info['type'] = type(obj)
     for i in dir(obj):
-        if i.startswith("__") or i.startswith("_"):
-            atr.append(i)
+        if callable(getattr(obj,i)):
+            method.append(i)
             info['attributes'] = atr
         else:
-            method.append(i)
+            atr.append(i)
             info['methods'] = method
     info['module'] = inspect.getmodule(introspection_info)
     return info
 
 
-
-number_info = introspection_info(23)
+number_info = introspection_info(32)
 pprint(number_info)
