653c25e6cc411256dbc5835c47f5908c460df451
Galina <116099759+GalinaLebedeva@users.noreply.github.com>
2024-09-27 14:25:41 +0300

    Update module_13_6.py
    Добавлен обработчик кнопки после ответа

--- module_13_6.py
+++ module_13_6.py
@@ -38,10 +38,12 @@ async def get_formulas(call):
     await call.message.answer('Обращаю Ваше внимание, что данная формула актуальна только для лиц в возрасте от 13 до 80 лет.\
                               Для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161.\
                               Для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+    await call.answer()
 
 @dp.callback_query_handler(text='calories')
 async def set_gender(call):
     await call.message.answer("Укажите свой пол в формате f / m")
+    await call.answer()
 
 @dp.message_handler(state=UserState.gender)
 async def set_age(message, state):
